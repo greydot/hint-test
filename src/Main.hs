3d60935f76557e2f43c0e0a6ca0f6ecc219333cf
Lana Black <lanablack@amok.cc>
2017-01-12 16:13:09 +0000

    Add prints.

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,20 @@ module Main where
 
 import Internal
 
+import Control.Monad.IO.Class
 import Language.Haskell.Interpreter
+
+say = liftIO . putStrLn
 
 compile :: String -> IO (Either InterpreterError (Int -> Int))
 compile b = runInterpreter $ do
+    say "Loading modules."
     loadModules ["Internal"]
+    say "Setting imports."
     setImports ["Prelude"
                ,"Internal"
                ]
+    say "Interpreting."
     interpret b id
 
 main :: IO ()
